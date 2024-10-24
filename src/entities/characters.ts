badb39830475b052be8823fba087cb7ab0e35adf
MedievalRain <medievalrain@gmail.com>
2024-10-25 00:09:01 +0300

    Simplify data

--- src/entities/characters.ts
+++ src/entities/characters.ts
@@ -1,68 +1,84 @@
 export type Character = {
-  character: string;
-  romaji: {
-    acceptedValues: string[];
-    renderedValue: string;
-  };
+  kana: string;
+  romaji: string;
 };
 
-export const hiragana: Character[] = [
-  { character: "あ", romaji: { acceptedValues: ["a"], renderedValue: "a" } },
-  { character: "い", romaji: { acceptedValues: ["i"], renderedValue: "i" } },
-  { character: "う", romaji: { acceptedValues: ["u"], renderedValue: "u" } },
-  { character: "え", romaji: { acceptedValues: ["e"], renderedValue: "e" } },
-  { character: "お", romaji: { acceptedValues: ["o"], renderedValue: "o" } },
-  { character: "か", romaji: { acceptedValues: ["ka"], renderedValue: "ka" } },
-  { character: "き", romaji: { acceptedValues: ["ki"], renderedValue: "ki" } },
-  { character: "く", romaji: { acceptedValues: ["ku"], renderedValue: "ku" } },
-  { character: "け", romaji: { acceptedValues: ["ke"], renderedValue: "ke" } },
-  { character: "こ", romaji: { acceptedValues: ["ko"], renderedValue: "ko" } },
-  { character: "さ", romaji: { acceptedValues: ["sa"], renderedValue: "sa" } },
-  {
-    character: "し",
-    romaji: { acceptedValues: ["shi"], renderedValue: "shi" },
-  },
-  { character: "す", romaji: { acceptedValues: ["su"], renderedValue: "su" } },
-  { character: "せ", romaji: { acceptedValues: ["se"], renderedValue: "se" } },
-  { character: "そ", romaji: { acceptedValues: ["so"], renderedValue: "so" } },
-  { character: "た", romaji: { acceptedValues: ["ta"], renderedValue: "ta" } },
-  {
-    character: "ち",
-    romaji: { acceptedValues: ["chi"], renderedValue: "chi" },
-  },
-  {
-    character: "つ",
-    romaji: { acceptedValues: ["tsu"], renderedValue: "tsu" },
-  },
-  { character: "て", romaji: { acceptedValues: ["te"], renderedValue: "te" } },
-  { character: "と", romaji: { acceptedValues: ["to"], renderedValue: "to" } },
-  { character: "な", romaji: { acceptedValues: ["na"], renderedValue: "na" } },
-  { character: "に", romaji: { acceptedValues: ["ni"], renderedValue: "ni" } },
-  { character: "ぬ", romaji: { acceptedValues: ["nu"], renderedValue: "nu" } },
-  { character: "ね", romaji: { acceptedValues: ["ne"], renderedValue: "ne" } },
-  { character: "の", romaji: { acceptedValues: ["no"], renderedValue: "no" } },
-  { character: "は", romaji: { acceptedValues: ["ha"], renderedValue: "ha" } },
-  { character: "ひ", romaji: { acceptedValues: ["hi"], renderedValue: "hi" } },
-  { character: "ふ", romaji: { acceptedValues: ["fu"], renderedValue: "fu" } },
-  { character: "へ", romaji: { acceptedValues: ["he"], renderedValue: "he" } },
-  { character: "ほ", romaji: { acceptedValues: ["ho"], renderedValue: "ho" } },
-  { character: "ま", romaji: { acceptedValues: ["ma"], renderedValue: "ma" } },
-  { character: "み", romaji: { acceptedValues: ["mi"], renderedValue: "mi" } },
-  { character: "む", romaji: { acceptedValues: ["mu"], renderedValue: "mu" } },
-  { character: "め", romaji: { acceptedValues: ["me"], renderedValue: "me" } },
-  { character: "も", romaji: { acceptedValues: ["mo"], renderedValue: "mo" } },
-  { character: "や", romaji: { acceptedValues: ["ya"], renderedValue: "ya" } },
-  { character: "ゆ", romaji: { acceptedValues: ["yu"], renderedValue: "yu" } },
-  { character: "よ", romaji: { acceptedValues: ["yo"], renderedValue: "yo" } },
-  { character: "ら", romaji: { acceptedValues: ["ra"], renderedValue: "ra" } },
-  { character: "り", romaji: { acceptedValues: ["ri"], renderedValue: "ri" } },
-  { character: "る", romaji: { acceptedValues: ["ru"], renderedValue: "ru" } },
-  { character: "れ", romaji: { acceptedValues: ["re"], renderedValue: "re" } },
-  { character: "ろ", romaji: { acceptedValues: ["ro"], renderedValue: "ro" } },
-  { character: "わ", romaji: { acceptedValues: ["wa"], renderedValue: "wa" } },
-  {
-    character: "を",
-    romaji: { acceptedValues: ["wo", "o"], renderedValue: "o" },
-  },
-  { character: "ん", romaji: { acceptedValues: ["n"], renderedValue: "n" } },
+export const hiraganaColumns: Character[][] = [
+  // First column (Vowels)
+  [
+    { kana: "あ", romaji: "a" },
+    { kana: "い", romaji: "i" },
+    { kana: "う", romaji: "u" },
+    { kana: "え", romaji: "e" },
+    { kana: "お", romaji: "o" },
+  ],
+  // Second column (K)
+  [
+    { kana: "か", romaji: "ka" },
+    { kana: "き", romaji: "ki" },
+    { kana: "く", romaji: "ku" },
+    { kana: "け", romaji: "ke" },
+    { kana: "こ", romaji: "ko" },
+  ],
+  // Third column (S)
+  [
+    { kana: "さ", romaji: "sa" },
+    { kana: "し", romaji: "shi" },
+    { kana: "す", romaji: "su" },
+    { kana: "せ", romaji: "se" },
+    { kana: "そ", romaji: "so" },
+  ],
+  // Fourth column (T)
+  [
+    { kana: "た", romaji: "ta" },
+    { kana: "ち", romaji: "chi" },
+    { kana: "つ", romaji: "tsu" },
+    { kana: "て", romaji: "te" },
+    { kana: "と", romaji: "to" },
+  ],
+  // Fifth column (N)
+  [
+    { kana: "な", romaji: "na" },
+    { kana: "に", romaji: "ni" },
+    { kana: "ぬ", romaji: "nu" },
+    { kana: "ね", romaji: "ne" },
+    { kana: "の", romaji: "no" },
+  ],
+  // Sixth column (H)
+  [
+    { kana: "は", romaji: "ha" },
+    { kana: "ひ", romaji: "hi" },
+    { kana: "ふ", romaji: "fu" },
+    { kana: "へ", romaji: "he" },
+    { kana: "ほ", romaji: "ho" },
+  ],
+  // Seventh column (M)
+  [
+    { kana: "ま", romaji: "ma" },
+    { kana: "み", romaji: "mi" },
+    { kana: "む", romaji: "mu" },
+    { kana: "め", romaji: "me" },
+    { kana: "も", romaji: "mo" },
+  ],
+  // Eighth column (Y)
+  [
+    { kana: "や", romaji: "ya" },
+    { kana: "ゆ", romaji: "yu" },
+    { kana: "よ", romaji: "yo" },
+  ],
+  // Ninth column (R)
+  [
+    { kana: "ら", romaji: "ra" },
+    { kana: "り", romaji: "ri" },
+    { kana: "る", romaji: "ru" },
+    { kana: "れ", romaji: "re" },
+    { kana: "ろ", romaji: "ro" },
+  ],
+  // Tenth column (W)
+  [
+    { kana: "わ", romaji: "wa" },
+    { kana: "を", romaji: "wo" },
+  ],
+  // Eleventh column (N)
+  [{ kana: "ん", romaji: "n" }],
 ];
