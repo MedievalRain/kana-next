9a83738eb3a07776e82da3004caa573e381bdff1
MedievalRain <medievalrain@gmail.com>
2024-10-26 21:25:07 +0300

    Fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,26 +7,19 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 
 export const metadata: Metadata = {
-  description: "Train Hiragana and Katakana",
-  title: "KanaNext",
+	description: "Train Hiragana and Katakana",
+	title: "KanaNext",
 };
-const nunito = Nunito({ display: "swap", preload: false });
+const nunito = Nunito({ display: "block", preload: false });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html>
-      <body
-        className={cn(
-          nunito.className,
-          "antialiased bg-background-100 selection:bg-[#42b4ff4d] text-[#C7CAD1]"
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html>
+			<body className={cn(nunito.className, "antialiased bg-background-100 selection:bg-[#42b4ff4d] text-[#C7CAD1]")}>{children}</body>
+		</html>
+	);
 }
