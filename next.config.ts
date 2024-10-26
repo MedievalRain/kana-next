f15bf79ba5a47ab846951cfa0ebb3161019cecd9
MedievalRain <medievalrain@gmail.com>
2024-10-26 21:42:17 +0300

    Update config for self hosting

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: "incremental",
-    reactCompiler: true,
-    typedRoutes: true,
-  },
+	experimental: {
+		ppr: "incremental",
+		reactCompiler: true,
+		typedRoutes: true,
+	},
+	output: "standalone",
 };
 
 export default nextConfig;
