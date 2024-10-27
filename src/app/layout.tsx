5f748ff454ea8f1b8da17da75d7f070411aa9c20
MedievalRain <medievalrain@gmail.com>
2024-10-27 07:55:53 +0300

    Add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 
-import { cn } from "@/utils/cn";
+import { Footer } from "@/components/footer";
 
 import "./globals.css";
 
+import { cn } from "@/utils/cn";
 import { Nunito } from "next/font/google";
 
 export const metadata: Metadata = {
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html>
-			<body className={cn(nunito.className, "antialiased bg-background-100 selection:bg-[#42b4ff4d] text-[#C7CAD1]")}>{children}</body>
+			<body className={cn(nunito.className, "antialiased bg-background-100 selection:bg-[#42b4ff4d] text-[#C7CAD1] pt-12 pb-6")}>
+				{children}
+				<Footer />
+			</body>
 		</html>
 	);
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+	return <footer className="mt-6 text-center text-sm text-gray-500">MedievalRain • 2024</footer>;
+};
